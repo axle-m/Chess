fb8b86b8aab3cbf1f8580c6cf668d43d0773c868
Alexander Marynchev <alemar27@bergen.org>
2024-07-10 14:37:31 -0400

    Methods to move pieces
    - move method to move piece and update board FEN
    - no script yet to check for move legality or to update all other FEN aspects

--- boychesser/Assets/Scenes/Scenes/Board Display/displayBoard.cs
+++ boychesser/Assets/Scenes/Scenes/Board Display/displayBoard.cs
@@ -33,6 +33,8 @@ public class displayBoard : MonoBehaviour
     //tiles
     public const float tileSize = 1.0f;
     Tile[] tiles = new Tile[64];
+    Tile selectedTile = null;
+    Tile targetTile = null;
 
 
 
@@ -46,7 +48,128 @@ public class displayBoard : MonoBehaviour
 
     private void Update()
     {
-       
+        move();
+    }
+
+    void move()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Input.mousePosition;
+            {
+
+                var mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                mouseWorldPos.z = 0f;              
+
+                //identify target tile
+                if(mouseWorldPos.x > -4.0f && mouseWorldPos.x < 4.0f && mouseWorldPos.y > -4.0f && mouseWorldPos.y < 4.0f)
+                {
+                    int file = Mathf.FloorToInt(0.5f + (mouseWorldPos.x + 4.0f) / tileSize);
+                    int rank = Mathf.FloorToInt(0.5f + (mouseWorldPos.y + 4.0f) / tileSize);
+
+                    if (file >= 0 && file < 8 && rank >= 0 && rank < 8)
+                    {
+
+                        //if no tile is selected, select the tile
+                        if (selectedTile == null)
+                        {
+                            selectedTile = tiles[file + rank * 8];
+                            Debug.Log(selectedTile.getCurPiece());
+
+                            //reset selected tile if no piece is on the tile
+                            if(selectedTile.getCurPiece() == '0')
+                            {
+                                selectedTile = null;
+                            }
+                        } 
+
+                        //if a tile is selected, move the piece from the selected tile to the target tile
+                        else if (selectedTile != null)
+                        {
+                            targetTile = tiles[file + rank * 8];
+                            Debug.Log(targetTile.getCurPiece());
+
+                            targetTile.changeCurPiece(selectedTile.getCurPiece());
+                            selectedTile.changeCurPiece('0');
+
+                            //reset selected and target tiles
+                            selectedTile = null;
+                            targetTile = null;
+
+                            //updaete fen
+                            updateFen();
+
+                            //replace pieces
+
+                            GameObject[] pieces = GameObject.FindGameObjectsWithTag("piece");
+                            foreach (GameObject obj in pieces)
+                            {
+                                Destroy(obj);
+                            }
+
+                            placePieces();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    void updateFen()
+    {
+        // Construct the board part of the FEN string
+        string boardFen = "";
+        int emptyCount = 0;
+
+        for (int rank = 7; rank >= 0; rank--)
+        {
+            for (int file = 0; file < 8; file++)
+            {
+                Tile currentTile = tiles[file + rank * 8];
+                char piece = currentTile.getCurPiece();
+
+                if (piece == '0')
+                {
+                    emptyCount++;
+                }
+                else
+                {
+                    if (emptyCount > 0)
+                    {
+                        boardFen += emptyCount.ToString();
+                        emptyCount = 0;
+                    }
+                    boardFen += piece;
+                }
+            }
+
+            if (emptyCount > 0)
+            {
+                boardFen += emptyCount.ToString();
+                emptyCount = 0;
+            }
+
+            if (rank > 0)
+            {
+                boardFen += "/";
+            }
+        }
+
+        // Extract the other parts of the previous FEN
+        string curFenString = curFen.ToString();
+        string[] fenParts = curFenString.Split(' ');
+        string activeColor = fenParts.Length > 1 ? fenParts[1] : "w";
+        string castlingAvailability = fenParts.Length > 2 ? fenParts[2] : "-";
+        string enPassantTarget = fenParts.Length > 3 ? fenParts[3] : "-";
+        string halfmoveClock = fenParts.Length > 4 ? fenParts[4] : "0";
+        string fullmoveNumber = fenParts.Length > 5 ? fenParts[5] : "1";
+
+        // Construct the full FEN string
+        string newFenString = boardFen + " " + activeColor + " " + castlingAvailability + " " + enPassantTarget + " " + halfmoveClock + " " + fullmoveNumber;
+        Fen newFen = new Fen(newFenString);
+
+        //update the current fen
+        curFen = newFen;
     }
 
     void placePieces()
@@ -65,61 +188,73 @@ public class displayBoard : MonoBehaviour
                 {
                     case 'K':
                         GameObject KingW = Instantiate(w_k, new Vector3((tile % 8) * tileSize - 4.0f, (int)(tile / 8) * tileSize - 4.0f, -1), Quaternion.identity);
+                        KingW.tag = "piece";
                         tiles[tile].changeCurPiece(row[j]);
                         tile++;
                         break;
                     case 'Q':
                         GameObject QueenW = Instantiate(w_q, new Vector3((tile % 8) * tileSize - 4.0f, (int)(tile / 8) * tileSize - 4.0f, -1), Quaternion.identity);
+                        QueenW.tag = "piece";
                         tiles[tile].changeCurPiece(row[j]);
                         tile++;
                         break;
                     case 'R':
                         GameObject RookW = Instantiate(w_r, new Vector3((tile % 8) * tileSize - 4.0f, (int)(tile / 8) * tileSize - 4.0f, -1), Quaternion.identity);
+                        RookW.tag = "piece";
                         tiles[tile].changeCurPiece(row[j]);
                         tile++;
                         break;
                     case 'B':
                         GameObject BishopW = Instantiate(w_b, new Vector3((tile % 8) * tileSize - 4.0f, (int)(tile / 8) * tileSize - 4.0f, -1), Quaternion.identity);
+                        BishopW.tag = "piece";
                         tiles[tile].changeCurPiece(row[j]);
                         tile++;
                         break;
                     case 'N':
                         GameObject KnightW = Instantiate(w_n, new Vector3((tile % 8) * tileSize - 4.0f, (int)(tile / 8) * tileSize - 4.0f, -1), Quaternion.identity);
+                        KnightW.tag = "piece";
                         tiles[tile].changeCurPiece(row[j]);
                         tile++;
                         break;
                     case 'P':
                         GameObject PawnW = Instantiate(w_p, new Vector3((tile % 8) * tileSize - 4.0f, (int)(tile / 8) * tileSize - 4.0f, -1), Quaternion.identity);
+                        PawnW.tag = "piece";
                         tiles[tile].changeCurPiece(row[j]);
                         tile++;
                         break;
                     case 'k':
                         GameObject KingB = Instantiate(b_k, new Vector3((tile % 8) * tileSize - 4.0f, (int)(tile / 8) * tileSize - 4.0f, -1), Quaternion.identity);
+                        KingB.tag = "piece";
                         tiles[tile].changeCurPiece(row[j]);
                         tile++;
                         break;
                     case 'q':
                         GameObject QueenB = Instantiate(b_q, new Vector3((tile % 8) * tileSize - 4.0f, (int)(tile / 8) * tileSize - 4.0f, -1), Quaternion.identity);
+                        QueenB.tag = "piece";
                         tiles[tile].changeCurPiece(row[j]);
                         tile++;
                         break;
                     case 'r':
                         GameObject RookB = Instantiate(b_r, new Vector3((tile % 8) * tileSize - 4.0f, (int)(tile / 8) * tileSize - 4.0f, -1), Quaternion.identity);
+                        RookB.tag = "piece";
                         tiles[tile].changeCurPiece(row[j]);
                         tile++;
                         break;
                     case 'b':
                         GameObject BishopB = Instantiate(b_b, new Vector3((tile % 8) * tileSize - 4.0f, (int)(tile / 8) * tileSize - 4.0f, -1), Quaternion.identity);
+                        BishopB.tag = "piece";
                         tiles[tile].changeCurPiece(row[j]);
                         tile++;
                         break;
                     case 'n':
                         GameObject KnightB = Instantiate(b_n, new Vector3((tile % 8) * tileSize - 4.0f, (int)(tile / 8) * tileSize - 4.0f, -1), Quaternion.identity);
+                        KnightB.tag = "piece";
                         tiles[tile].changeCurPiece(row[j]);
                         tile++;
                         break;
                     case 'p':
                         GameObject PawnB = Instantiate(b_p, new Vector3((tile % 8) * tileSize - 4.0f, (int)(tile / 8) * tileSize - 4.0f, -1), Quaternion.identity);
+                        PawnB.tag = "piece";
                         tiles[tile].changeCurPiece(row[j]);
                         tile++;
                         break;
