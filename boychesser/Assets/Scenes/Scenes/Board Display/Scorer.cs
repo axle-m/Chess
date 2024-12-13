8b225eee03e5de2c904ae9c1bc6788d6b5938fa3
gabmoj27 <gabmoj27@bergen.org>
2024-12-13 13:13:00 -0500

    Made Basic Scorer
    It takes the amount of pieces each side has and calculates whose winning. We can use this to make the bot go through the legal moves and evaluate the best future position and play that move.

--- boychesser/Assets/Scenes/Scenes/Board Display/Scorer.cs
+++ boychesser/Assets/Scenes/Scenes/Board Display/Scorer.cs
@@ -1,14 +1,75 @@
 using System.Collections;
 public class Scorer : Board{
-    readonly int qScore = 9, rScore = 5, nScore = 3, bScore = 3, pScore = 1;
-    char[] wPieces = new char[] { 'q', 'r', 'r', 'b', 'b', 'n', 'n', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p' };
-    char[] bPieces = new char[] { 'q', 'r', 'r', 'b', 'b', 'n', 'n', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p' };
-    public int getScore(string curFen)
-    {
-        int score = 0;
-        char[] fenArr = curFen.ToCharArray();
-        var fenList = new ArrayList();
-        foreach (char c in fenArr)
+    // readonly int qScore = 9, rScore = 5, nScore = 3, bScore = 3, pScore = 1;
+    // char[] wPieces = new char[] { 'Q', 'R', 'R', 'B', 'B', 'N', 'N', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P' };
+    // char[] bPieces = new char[] { 'q', 'r', 'r', 'b', 'b', 'n', 'n', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p' };
+    public int getBasicScore(string curFen){  //yalls methods are dogshit, this is good
+        int whiteScore = 0;
+        int blackScore = 0;
+        int i = 0;
+        while(curFen[i] != ' '){
+            if(char.IsLetter(curFen[i])){
+                if(curFen[i] == 'K'){
+                    whiteScore +=90;
+                    i++;
+                }
+                if(curFen[i] == 'Q'){
+                    whiteScore +=9;
+                    i++;
+                }
+                else if(curFen[i] == 'R'){
+                    whiteScore +=5;
+                    i++;
+                }
+                else if(curFen[i] == 'B'){
+                    whiteScore +=3;
+                    i++;
+                }
+                else if(curFen[i] == 'N'){
+                    whiteScore +=3;
+                    i++;
+                }
+                else if(curFen[i] == 'P'){
+                    whiteScore +=1;
+                }
+                if(curFen[i] == 'k'){
+                    whiteScore +=90;
+                    i++;
+                }
+                else if(curFen[i] == 'q'){
+                    blackScore +=9;
+                    i++;
+                }
+                else if(curFen[i] == 'r'){
+                    blackScore +=5;
+                    i++;
+                }
+                else if(curFen[i] == 'b'){
+                    blackScore +=3;
+                    i++;
+                }
+                else if(curFen[i] == 'n'){
+                    blackScore +=3;
+                    i++;
+                }
+                else if(curFen[i] == 'p'){
+                    blackScore +=1;
+                    i++;
+                }                                                                
+            else{
+                i++;
+            }
+            }
+        }
+        return whiteScore - blackScore;
+    }
+
+    
+        
+        //var fenList = new ArrayList();
+        
+        
+        /* foreach (char c in fenArr)
         {
             if (char.IsLetter(c))
             {
@@ -27,7 +88,9 @@ public class Scorer : Board{
             }
         }
         
-        return 0;
+        return 0;     THAT WAS KAI'S EDIT, BUT GABE AND MAX DIDN'T LIKE IT
+        */ 
+        
         /*
         int score = 0;
         if(ActiveColor != 'w' && ActiveColor != 'b')
@@ -68,5 +131,5 @@ public class Scorer : Board{
             return ' ';
         }
         */
-    }
 }
+
