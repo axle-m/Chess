b4ec72dadce8971ac99306a7ddd35085aff370df
kai-ai-ver32 <kai-ai-ver32>
2025-01-13 08:07:11 -0500

    Added text mesh pro and a score board file for displaying each score

---
+++ boychesser/Assets/Scenes/Scenes/Board Display/scoreBoard.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scoreBoard : MonoBehaviour
+{
+    public string whiteScoreBoard;
+    public string blackScoreBoard;
+    public double whiteScore;
+    public double blackScore;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        whiteScoreBoard = "White Score: " + whiteScore;
+        blackScoreBoard = "Black Score: " + blackScore;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        whiteScore = Scorer.getPieceScore();
+    }
+}
