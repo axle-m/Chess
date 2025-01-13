4c3daf485210bee802bed20ef785c704ec4af211
MaxN1322 <maxner27@bergen.org>
2025-01-13 18:41:02 -0500

    I clutched up
    We need something that actually ends the game. This took a while, so we probably should look over this.

--- boychesser/Assets/Scenes/Scenes/Board Display/ChessBot.cs
+++ boychesser/Assets/Scenes/Scenes/Board Display/ChessBot.cs
@@ -26,64 +26,22 @@ public class ChessBot
     }
 
     // Selects a random legal move and logs it
-    public static string randomMove(String[] moves){
-        System.Random rnd = new System.Random();
-        string move = moves[rnd.Next(0, moves.Length)];
-        Debug.Log(move + " bot move");//maybe instead of move add something more specific like "Evaluating move (currentLegalMoves[i] with score {score}");
-        return move;
+    public static string playBestMove(String[] moves, String fenString) {
+        String move = GetBestMove(moves, fenString);
+        Debug.Log($"Bot move: {move}");   
+        return move;  
     }
 
-   
-    string bestPositionMove(String[] currentLegalMoves, String fenString){
-        //Make a 2d array
-        //Inside each array, first index is the move, the second index is the value
-        //Store them as strings, String.GetNumbericalValue to get the second value
-
-        List<string[]> movesAndValueList = new List<string[]>();
-        //int val = int.Parse(ints[index][1]);
-        double highestValue = 0;
-        int index = 0;
-        if(currentLegalMoves.Length == 0){
-            Debug.LogWarning("No legal moves available");
-            return null;
-        }
-        for(int i = 0; i < currentLegalMoves.Length; i++){
-            Fen move = new Fen(Fen.move(fenString, currentLegalMoves[i]));
-            double moveValue = getPositionScoring(move);
-            if(moveValue > highestValue){
-                highestValue = moveValue;
-                index = i;
-            }
-            string[] movesAndValueArray = {currentLegalMoves[i], moveValue.ToString()};
-            movesAndValueList.Add(movesAndValueArray);
-        }
-        return currentLegalMoves[index];
-
-        //Goes through all legal moves, Applies scorer, stores move, returns move with highest value
-
-
-        //This returns the best possible move based on position however more can be done
-        //We can use the 2d array (movesAndValueList) choose between the best 1-3 or 1-5 
-        //It should still favor the best move  the probability could be something like ⌄⌄⌄
-        //(1st - 70%, 2nd - 15%, 3rd - 5%, 4th - 5%, 5th - 5%)?
-        //We could keep track of those indicies or sort the arrays to figure this out
-
-        //Question: Should this return a string (move played) or fen (curFen + move played)?
-    }
-    // HEY GUYS READ THIS: how about we compress the best position move and the best piece move into one class?
-    /*this is a possible implementation of this: 
-    string GetBestMove(LegalMovesList legalMovesList, Func<Fen, double> scoringFunction){
+    public static string GetBestMove(String[] currentLegalMoves, String fenString)
+{
     double highestValue = double.MinValue;
     int index = -1;
-    string[] currentLegalMoves = (string[])legalMovesList.getLegalMoves(board.curFen);
-    string fenString = board.curFen;
 
-
+    // Create the combined scoring function by using both piece and position scoring
     for (int i = 0; i < currentLegalMoves.Length; i++)
     {
-        Fen move = new Fen(Fen.move(fenString, currentLegalMoves[i]));
-        double score = scoringFunction(move);
-
+        Fen moveFen = new Fen(Fen.move(fenString, currentLegalMoves[i]));
+        double score = combinedScoring(moveFen);  // Directly use the combinedScoring
 
         if (score > highestValue)
         {
@@ -92,45 +50,19 @@ public class ChessBot
         }
     }
 
+    return index >= 0 ? currentLegalMoves[index] : null;
+}
 
-    return index >= 0 ? currentLegalMoves[index] : null; // Handle no valid moves
-}*/
-// ALSO we should also be able to call this new class with something like string bestPosition(or)PieceMove = GetBestMove(legalMovesList, getPositionScoring);
-
-
-
-
-
-    string bestPieceMove(String[] currentLegalMoves, String fenString)
-    {
-        double highestValue = 0;
-        int index = 0;
-        List<string[]> movesAndValueList = new List<string[]>();
-        if(currentLegalMoves.Length == 0){
-            Debug.LogWarning("No legal moves available");  //ADDED THIS just in case of checkmate bugs - Max
-                                                           //good forsight but i dont have checks implemented and i think we'll stick to a pseudo legal moves list where a position without a king is checkmate and the bot scoring algorithm should prevent it from giving up the king
-            return null;
-        }
-        for(int i = 0; i < currentLegalMoves.Length; i++){
-            Fen move = new Fen(Fen.move(fenString, currentLegalMoves[i]));
-            double moveValue = getPieceScoring(move);
-            if(moveValue > highestValue){
-                highestValue = moveValue;
-                index = i;
-            }
-            string[] movesAndValueArray = {currentLegalMoves[i], moveValue.ToString()};
-            movesAndValueList.Add(movesAndValueArray);
-        }
-        return currentLegalMoves[index];
-        //This is a copy-paste of the position mover just different scorer used
+    public static double combinedScoring(Fen fen) {
+    double materialScore = getPieceScoring(fen);
+    double positionScore = getPositionScoring(fen);
+    return materialScore + positionScore; 
+}
+    public static double getPositionScoring(Fen fen) {
+        return Scorer.getPositionScore(fen);
     }
 
-
-    public double getPositionScoring(Fen fen){
-        return Scorer.getPositionScore(fen); 
-    }
-
-    public double getPieceScoring(Fen fen){
-        return Scorer.getPieceScore(fen); 
+    public static double getPieceScoring(Fen fen) {
+        return Scorer.getPieceScore(fen);
     }
 }
