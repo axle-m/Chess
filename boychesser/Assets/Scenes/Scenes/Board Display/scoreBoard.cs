6085386988c9d78734ab763a6a086c320f277cf1
kai-ai-ver32 <kai-ai-ver32>
2025-01-13 08:54:03 -0500

    fixed stuff

--- boychesser/Assets/Scenes/Scenes/Board Display/scoreBoard.cs
+++ boychesser/Assets/Scenes/Scenes/Board Display/scoreBoard.cs
@@ -1,12 +1,13 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class scoreBoard : MonoBehaviour
+public class scoreBoard : Board
 {
     public string whiteScoreBoard;
     public string blackScoreBoard;
     public double whiteScore;
     public double blackScore;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,6 +18,11 @@ public class scoreBoard : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        whiteScore = Scorer.getPieceScore();
+        if(curFen.getActiveColor() == "w"){
+            whiteScore = Scorer.getPieceScore(curFen);
+            } 
+        else{
+            blackScore = Scorer.getPieceScore(curFen);
+            }
     }
 }
