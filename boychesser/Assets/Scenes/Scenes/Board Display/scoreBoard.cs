79f852e45186b1d3554527b2ed38a521479b6476
kai-ai-ver32 <kai-ai-ver32>
2025-01-14 13:29:42 -0500

    scoreboard works

--- boychesser/Assets/Scenes/Scenes/Board Display/scoreBoard.cs
+++ boychesser/Assets/Scenes/Scenes/Board Display/scoreBoard.cs
@@ -1,18 +1,19 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class scoreBoard : Board
 {
-    public string whiteScoreBoard;
-    public string blackScoreBoard;
+    public TMP_Text whiteScoreBoard;
+    public TMP_Text blackScoreBoard;
     public double whiteScore;
     public double blackScore;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        whiteScoreBoard = "White Score: " + whiteScore;
-        blackScoreBoard = "Black Score: " + blackScore;
+        whiteScoreBoard.text = "White Score: " + whiteScore;
+        blackScoreBoard.text = "Black Score: " + blackScore;
     }
 
     // Update is called once per frame
