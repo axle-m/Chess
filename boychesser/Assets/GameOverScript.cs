5aa6b74645f061b9be543d890a86214ef4615798
kailindemer <kailin27@bergen.org>
2025-01-17 02:03:34 -0500

    finished game over screen

--- boychesser/Assets/GameOverScript.cs
+++ boychesser/Assets/GameOverScript.cs
@@ -1,16 +1,12 @@
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro;
 public class GameOverScript : MonoBehaviour
 {
+    public TMP_Text Moves;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void Setup(int moves) {
+        gameObject.SetActive(true);
+        Moves.text = moves.ToString() + "MOVES";
     }
 }
